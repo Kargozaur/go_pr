48499ec12777fe26afe8abf328e75027b5cc4e9e
Illia <kargozaurus@gmail.com>
2026-06-01 00:07:03 +0400

    attached middleware

--- services/gateway/router/router.go
+++ services/gateway/router/router.go
@@ -1,12 +1,13 @@
 package router
 
 import (
+	"ecommerce/gateway/middleware"
 	"net/http"
 )
 
 func RegisterUserRouter(mux *http.ServeMux) {
 	mux.HandleFunc("POST /users/register", registerUser)
 	mux.HandleFunc("POST /users/login", loginUser)
-	mux.HandleFunc("POST /users/logout/single", logoutUserSingle)
-	mux.HandleFunc("POST /users/logout/all", logoutUserAll)
+	mux.Handle("POST /users/logout/single", middleware.VerifyAccessToken(http.HandlerFunc(logoutUserSingle)))
+	mux.Handle("POST /users/logout/all", middleware.VerifyAccessToken(http.HandlerFunc(logoutUserAll)))
 }
