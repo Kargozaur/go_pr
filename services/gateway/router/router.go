5efa3065162c6e19b24145bf99015c7e9c8740b3
Illia <kargozaurus@gmail.com>
2026-05-31 22:45:44 +0400

    login

--- services/gateway/router/router.go
+++ services/gateway/router/router.go
@@ -5,5 +5,6 @@ import (
 )
 
 func RegisterUserRouter(mux *http.ServeMux) {
-	mux.HandleFunc("POST /users", registerUser)
+	mux.HandleFunc("POST /users/register", registerUser)
+	mux.HandleFunc("POST /users/login", loginUser)
 }
--- services/gateway/router/user_handler.go
+++ services/gateway/router/user_handler.go
@@ -12,6 +12,7 @@ import (
 )
 
 func registerUser(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 	w.Header().Set("Content-Type", "application/json")
 	var userStruct schemas.User
 	if err := json.NewDecoder(r.Body).Decode(&userStruct); err != nil {
@@ -36,11 +37,50 @@ func registerUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusCreated {
 		http.Error(w, resp.Status, resp.StatusCode)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
 	w.Write(userBytes)
+}
+
+func loginUser(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	w.Header().Set("Content-Type", "application/json")
+	var loginStruct schemas.LoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&loginStruct); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	loginBytes, err := json.Marshal(loginStruct)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*5))
+	link := fmt.Sprintf("http://%s:8002/users/login", os.Getenv("HOST"))
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "POST", link, bytes.NewBuffer(loginBytes))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		http.Error(w, resp.Status, resp.StatusCode)
+		return
+	}
+	bodyBytes, err := json.Marshal(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(bodyBytes)
 }
