711102d6e4232938c4666f6129e4b29d277d41b2
Illia <kargozaurus@gmail.com>
2026-06-01 21:35:33 +0400

    user service request timeout

--- services/user-service/internal/reqhandler/user.go
+++ services/user-service/internal/reqhandler/user.go
@@ -1,6 +1,7 @@
 package reqhandler
 
 import (
+	"context"
 	"ecommerce/pkg/logger"
 	"ecommerce/user-service/internal/jw"
 	"ecommerce/user-service/internal/phasher"
@@ -8,8 +9,10 @@ import (
 	"ecommerce/user-service/internal/svc"
 	"ecommerce/user-service/internal/thasher"
 	"ecommerce/user-service/internal/util/validator"
+	"errors"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/uptrace/bun"
@@ -30,13 +33,15 @@ func NewHandler(db *bun.DB, logger *logger.Logger) *Handler {
 }
 
 func (h *Handler) Register(c *gin.Context) {
+	ctx, cancel := h.prepareContext(c)
+	defer cancel()
 	var userBody schemas.RegisterSchema
 	if err := c.ShouldBindJSON(&userBody); err != nil {
 		h.logger.Writer.Error("bind fail", "error", err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	if err := h.service.Register(c.Request.Context(), userBody); err != nil {
+	if err := h.service.Register(ctx, userBody); err != nil {
 		h.logger.Writer.Error("register error", "error", err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -45,14 +50,20 @@ func (h *Handler) Register(c *gin.Context) {
 }
 
 func (h *Handler) Login(c *gin.Context) {
+	ctx, cancel := h.prepareContext(c)
+	defer cancel()
 	var userBody schemas.LoginSchema
 	if err := c.ShouldBindJSON(&userBody); err != nil {
 		h.logger.Writer.Error("bind fail", "error", err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	tokenPair, err := h.service.Login(c.Request.Context(), userBody)
+	tokenPair, err := h.service.Login(ctx, userBody)
 	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			c.JSON(http.StatusRequestTimeout, gin.H{"error": "request timeout"})
+			return
+		}
 		h.logger.Writer.Error("login fail", "error", err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -63,13 +74,19 @@ func (h *Handler) Login(c *gin.Context) {
 }
 
 func (h *Handler) LogoutAll(c *gin.Context) {
+	ctx, cancel := h.prepareContext(c)
+	defer cancel()
 	userID, ok := c.Get("userID")
 	if !ok {
 		h.logger.Writer.Error("login fail", "error", "failed to get user id from middleware")
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Failed to read token"})
 		return
 	}
-	if err := h.service.Logout(c.Request.Context(), userID); err != nil {
+	if err := h.service.Logout(ctx, userID); err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			c.JSON(http.StatusRequestTimeout, gin.H{"error": "request timeout"})
+			return
+		}
 		h.logger.Writer.Error("failed to delete token from db", "error", err.Error())
 		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
@@ -78,13 +95,19 @@ func (h *Handler) LogoutAll(c *gin.Context) {
 }
 
 func (h *Handler) Logout(c *gin.Context) {
+	ctx, cancel := h.prepareContext(c)
+	defer cancel()
 	token, ok := c.Get("token")
 	if !ok {
 		h.logger.Writer.Error("login fail", "error", "failed to get user token from middleware")
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Failed to read token"})
 		return
 	}
-	if err := h.service.Logout(c.Request.Context(), token); err != nil {
+	if err := h.service.Logout(ctx, token); err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			c.JSON(http.StatusRequestTimeout, gin.H{"error": "request timeout"})
+			return
+		}
 		h.logger.Writer.Error("failed to delete token from db", "error", err.Error())
 		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
@@ -96,17 +119,28 @@ func (h *Handler) Logout(c *gin.Context) {
 }
 
 func (h *Handler) GetProfile(c *gin.Context) {
+	ctx, cancel := h.prepareContext(c)
+	defer cancel()
 	userID, ok := c.Get("userID")
 	if !ok {
 		h.logger.Writer.Error("failed to get user id from middleware", "error", "user id not found")
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "User ID not found"})
 		return
 	}
-	profile, err := h.service.GetProfile(c.Request.Context(), userID)
+	profile, err := h.service.GetProfile(ctx, userID)
 	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			c.JSON(http.StatusRequestTimeout, gin.H{"error": "request timeout"})
+			return
+		}
 		h.logger.Writer.Error("failed to get profile", "error", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, profile)
+}
+
+func (h *Handler) prepareContext(c *gin.Context) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(c.Request.Context(), time.Duration(time.Second*7))
+	return ctx, cancel
 }
