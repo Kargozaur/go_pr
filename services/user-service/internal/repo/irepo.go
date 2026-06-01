2b589c439ee00ca65097f0e3993de431be689445
Illia <kargozaurus@gmail.com>
2026-06-01 20:07:23 +0400

    interfaces

--- services/user-service/internal/repo/irepo.go
+++ services/user-service/internal/repo/irepo.go
@@ -6,11 +6,30 @@ import (
 	"github.com/uptrace/bun"
 )
 
-// General interface to describe the repository. It may be separated into 4 different
-// interfaces to follow the Interface segregation principle in SOLID.
-type IRepo interface {
+type Create interface {
 	Create(context.Context, RepoType, bun.IDB) (any, error)
+}
+
+type Read interface {
 	Read(context.Context, any) (any, error)
+}
+
+type Update interface {
 	Update(context.Context, any, RepoType, bun.IDB) (any, error)
+}
+
+type Delete interface {
 	Delete(context.Context, any, bun.IDB) (bool, error)
+}
+
+type CRU interface {
+	Create
+	Read
+	Update
+}
+
+type CRD interface {
+	Create
+	Read
+	Delete
 }
